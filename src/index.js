fcdd4a0d8005da9683fff1ce4a51528860cb360c
devwithusman <153663521+devwithusman@users.noreply.github.com>
2023-12-12 22:44:20 +0400

    new change in index

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 const { app } = require('./app');
 
+
+console.log('i am usman rocket');
+
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
   console.log(`Running on http://localhost:${port}`);
